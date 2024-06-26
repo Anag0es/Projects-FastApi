f9c8683bfb0a635d14bf38e3e0fc1a5f46da7d1a
Anag0es <analuisa.goes2004@gmail.com>
2024-06-26 19:10:38 -0300

    feature: test another types of responses

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+from fastapi.responses import HTMLPResponse
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/aula01')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+# retornar um HTML
+# status_code -> o que queremos que seja retornado
+# response_class -> o tipo de resposta que queremos que seja retornado
+@app.get('/aula02', status_code=HTTPStatus.OK, response_class=HTMLPResponse)
+def read_root():
+    return """
+    <html>
+        <head>
+            <title>Olá Mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo!</h1>
+        </body>
+        </html>"""
