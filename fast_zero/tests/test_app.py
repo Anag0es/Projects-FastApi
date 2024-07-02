4d57cde9e65d76b2d4044608d6280540a703898d
Anag0es <analuisa.goes2004@gmail.com>
2024-07-02 19:14:02 -0300

    Feature: curso_projeto - FastApi

---
+++ fast_zero/tests/test_app.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app) # Arrange (Preparação)
+
+    # Act (Ação)
+    response = client.get('/')
+
+    # Assert (Verificação)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
+    
+    # teardown (Limpeza) - Não é necessário
